642c09bed34f7a0118df3b8be75faaee6f6f9446
hye-w-on <dpaak1@gmail.com>
2024-01-05 16:42:55 +0900

    로딩바 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import React, { useReducer } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useEffect, useReducer } from 'react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './react-query/queryClient';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { getSessionStorageInfo } from 'utils/sessionStorageUtil';
+import useEmployeeSessionStore from 'store/EmployeeSessionStore';
+
+import sessionApi from 'rest-api/session';
+
 //Pages
-import Loading from './pages/layout/Loading';
-import CommonAlert from './pages/layout/CommonAlert';
+import ReactQueryLoading from './pages/layout/ReactQueryLoading';
+import CommonSnackbar from './pages/layout/CommonSnackbar';
 import Router from './routers/Router';
 import HeaderBar from './pages/layout/HeaderBar';
 import HorizonMenu from './pages/layout/HorizonMenu';
 //style
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import { colorTheme } from './components/Color';
-
+import CommonLoading from 'pages/layout/CommonLoading';
 function App() {
+  const { employeeId, setEmployeeSession } = useEmployeeSessionStore();
+
+  useEffect(() => {
+    if (!employeeId) {
+      (async () => {
+        //cookie로 조회
+        const response = await sessionApi.getEmployeeSession();
+        if (response.successOrNot === 'Y' && response?.data) {
+          const session = response.data;
+          setEmployeeSession(session);
+          console.log('auto login success');
+        } else {
+          console.log('auto login fail');
+          //navigate('/login', { replace: true });
+        }
+      })();
+
+      //        const sessionStorageInfo = getSessionStorageInfo();
+      //        const { successOrNot, data } = await sessionApi.autoLogin(idToken);
+      //        router.reload(window.location.pathname);
+    }
+  }, []);
   return (
     <>
       <ThemeProvider theme={colorTheme}>
         <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <HeaderBar />
-            <HorizonMenu />
-            <Loading />
-            <Router />
-            <CommonAlert />
-            <ReactQueryDevtools />
-          </BrowserRouter>
+          <HeaderBar />
+          <HorizonMenu />
+          <ReactQueryLoading />
+          <CommonLoading />
+          <Router />
+          <CommonSnackbar />
+          <ReactQueryDevtools />
         </QueryClientProvider>
       </ThemeProvider>
     </>
